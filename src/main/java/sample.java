35beb988f107b0c4c5dfbbc50b48e179fa14c8a5
satish <vadlamurisatish@gmail.com>
2019-01-23 08:51:37 +0530

    sample

---
+++ src/main/java/sample.java
@@ -0,0 +1,20 @@
+
+/**
+ *
+ * @author vadla
+ */
+public class sample {
+
+    public static void main(String[] args) {
+
+       int num = 10, count = 1, total = 0;
+
+       while(count <= num)
+       {
+           total = total + count;
+           count++;
+       }
+
+       System.out.println("Sum of first 10 natural numbers is: "+total);
+    }
+}
